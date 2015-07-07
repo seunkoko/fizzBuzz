3d9eeea4dc76df937965d8eb0fecd4075b7fead7
seunkoko <seunowonikoko@gmail.com>
2015-07-07 20:23:19 +0100

    The code has been refractored as taught to me by Mr. Jacob

--- return_fizzbuzz.js
+++ return_fizzbuzz.js
@@ -1,11 +1,24 @@
+
 for (var i = 1; i <= 20; i++){
-    if(i % 3 === 0 && i % 5 === 0){
+    if(check_both(i)){
         console.log("fizzbuzz")
-    }else if(i % 3 === 0){
+    }else if(check3(i)){
         console.log("fizz");
-    }else if(i % 5 === 0){
+    }else if(check5(i)){
         console.log("buzz");
     }else{
         console.log(i);
     }
+}
+
+function check_both(value){
+    return (value % 3 === 0 && value % 5 === 0);
+}
+
+function check3(value){
+    return (value % 3 === 0);
+}
+
+function check5(value){
+    return (value % 5 === 0);
 }
